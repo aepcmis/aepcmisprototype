3141948f4a9394cc79c1b02f044b39fef5b2b744
unknown <paudelp@gmail.com>
2013-12-24 15:47:03 +0545

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aepcmisprototype::Application.config.secret_key_base = '37fed2f2b75721ae075d38b02343f5cfacad3209203ae3f87b714d8d62227ed70042372920bc89746ddb31f71eed5965bc40b6cb631d375dca3e406b17447c56'
